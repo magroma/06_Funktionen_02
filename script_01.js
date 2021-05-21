53196acd5046d26f686a72e10e32cddb4a4d1316
Marco <marco_2@gmx.de>
2021-05-21 12:19:28 +0200

    Auswahl der Rechenart mit "switch"

--- script_01.js
+++ script_01.js
@@ -3,10 +3,37 @@
 /*
 0. a+b / a-b/ a*b / a/b  // ergebnis c
 1. Dateneingabe + -überprüfung :: 
-2. Auswahl Rechenart :: 
-3. Fkt. Grundrechenarten :: 
+2. Auswahl Rechenart :: check!
+3. Fkt. Grundrechenarten :: check!
 4. Ausgabe in Konsole :: check!
 */
+
+// Modul: Auswahl der Rechenart | Test:
+// ausgabe(rechner("+",2,2));
+// ausgabe(rechner("-",8,2));
+// ausgabe(rechner("*",3,4));
+// ausgabe(rechner("/",25,5));
+// ausgabe(rechner("/",25,0));
+// ausgabe(rechner("x",25,5));
+function rechner(op,a,b) {
+
+    switch (op) {
+        case "+":
+            return addieren(a,b);
+        case "-":
+            return subtrahieren(a,b);
+        case "*":
+            return multiplizieren(a,b);
+        case "/":
+            return dividieren(a,b);
+        default:
+            return "Irgendwas ist schiefgelaufen!"
+    }
+
+    // return dividieren(a,b);
+    // return multiplizieren(a,b);
+}
+
 
 // Modul: Addition a + b | Test:
 // ausgabe(addieren(parseFloat(prompt("Erste Zahl?")),parseFloat(prompt("Zweite Zahl?"))));
